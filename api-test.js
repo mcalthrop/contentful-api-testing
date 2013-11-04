291f9140e9771c85db5322e5cd41d850a6f0efec
Matt Calthrop <mcalthrop@gmail.com>
2013-11-04 12:47:22 +0000

    Modified JS to nest config object

--- api-test.js
+++ api-test.js
@@ -6,33 +6,27 @@
         log = require('log'),
         protocol = 'http',
         cdnHost = 'cdn.contentful.com',
-        srcConfig = {
-            protocol: protocol,
-            cdnHost: cdnHost,
-            spaceName: 'cms-master',
-            spaceId: 'e2h9ve8r21wy',
-            accessToken: '5e4de9569f077ad4c4f8eda84305d3ae73284adc0e15a8df2fe7151f0fe318ba'
-        },
-        destConfig = {
-            protocol: protocol,
-            cdnHost: cdnHost,
-            spaceName: 'cms-test',
-            spaceId: '',
-            accessToken: ''
+        source = {
+            config: {
+                protocol: protocol,
+                cdnHost: cdnHost,
+                spaceName: 'cms-master',
+                spaceId: 'e2h9ve8r21wy',
+                accessToken: '5e4de9569f077ad4c4f8eda84305d3ae73284adc0e15a8df2fe7151f0fe318ba'
+            }
         };
 
-    var srcClient = contentful.createClient({
-        accessToken: srcConfig.accessToken,
-        space: srcConfig.spaceId
+    source.client = contentful.createClient({
+        accessToken: source.config.accessToken,
+        space: source.config.spaceId
     });
+    console.log(source.client);
 
-    var srcSpace = srcClient.space().then(log.ok, log.fail);
+    source.space = source.client.space().then(log.ok, log.fail);
+    console.log(source.space);
 
-    var srcContentTypes = srcClient.contentTypes().then(log.ok, log.fail);
-
-    console.log(srcClient);
-    console.log(srcSpace);
-    console.log(srcContentTypes);
+    source.contentTypes = source.client.contentTypes().then(log.ok, log.fail);
+    console.log(source.contentTypes);
 
 }());
 
