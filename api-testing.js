79c99d0ae13bbf888ed1c373fcf315974edef978
Matt Calthrop <mcalthrop@gmail.com>
2013-11-05 08:49:18 +0000

    Modified access token string

--- api-testing.js
+++ api-testing.js
@@ -12,7 +12,7 @@
                 host: cdnHost,
                 spaceName: 'cms-master',
                 spaceId: 'e2h9ve8r21wy',
-                accessToken: '5e4de9569f077ad4c4f8eda84305d3ae73284adc0e15a8df2fe7151f0fe318ba'
+                accessToken: 'REPLACE_WITH_VALID_ACCESS_TOKEN'
             }
         };
 
