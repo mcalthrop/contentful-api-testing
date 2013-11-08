ac69010c2cfe8e673002828e0729a1e131f69bc5
Matt Calthrop <mcalthrop@gmail.com>
2013-11-08 13:16:27 +0000

    NodeJS script now working

--- api-testing.js
+++ api-testing.js
@@ -1,34 +1,59 @@
+/*global require */
+
 // Testing the Contentful API with Javascript.
 
 (function () {
 
+    /**
+     * Utility function to retrieve the value of an environment variable, and optionally
+     * throw an error if the specified variable has not been set.
+     *
+     * @param {String} varName name of environment variable to retrieve
+     * @param {Boolean} throwErrorIfUndefined if the specified variable has not been defined
+     *
+     * @returns {String} the value of the specified env variable
+     */
+    function getEnv(varName, throwErrorIfUndefined) {
+        var varValue = process.env[varName];
+
+        if (varValue === undefined && throwErrorIfUndefined) {
+            throw new ReferenceError('Environment variable is not defined: ' + varName);
+        }
+
+        return varValue;
+    }
+
     var contentfulManagement = require('contentful-management'),
-        log = require('log'),
+        log = console.log.bind(console),
         protocol = 'http',
-        cdnHost = 'cdn.contentful.com',
         source = {
             config: {
                 protocol: protocol,
-                host: cdnHost,
-                spaceName: 'REPLACE_WITH_VALID_SPACE_NAME',
-                spaceId: 'REPLACE_WITH_VALID_SPACE_ID',
-                accessToken: 'REPLACE_WITH_VALID_ACCESS_TOKEN'
+                host: getEnv('CONTENTFUL_MANAGEMENT_HOSTNAME', true),
+                spaceId: getEnv('CONTENTFUL_SPACE_ID', true),
+                accessToken: getEnv('CONTENTFUL_ACCESS_TOKEN', true)
             }
         };
+
+    log("Source:", source);
 
     source.client = contentfulManagement.createClient({
         accessToken: source.config.accessToken,
         host: source.config.host,
         secure: false
     });
-    console.log(source.client);
 
-    source.space = source.client.getSpace().then(log.ok, log.fail);
-    console.log(source.space);
+    log('Source client:', source.client);
 
-    source.contentTypes = source.client.contentTypes().then(log.ok, log.fail);
-    console.log(source.contentTypes);
-
+    source.client.getSpace(source.config.spaceId).then(
+        function (space) {
+            log('Source getSpace OK:', space);
+            source.space = space;
+        },
+        function () {
+            log('Source getSpace ERROR:', arguments);
+        }
+    );
 }());
 
 // EOF
