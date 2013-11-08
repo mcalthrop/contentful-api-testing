f966323f76886d172b8f705ab1755a7e1cb70f9d
Matt Calthrop <mcalthrop@gmail.com>
2013-11-08 13:08:54 +0000

    Added env-setup.bash

---
+++ env-setup.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# ------------------------------------------------------------------------------------------
+#
+# Sample bash script for setting up the environment before running api-testing.js.
+#
+# Usage:
+#   Change the environment variables in this file to correct values, then run:
+#    . env-setup.bash
+#
+# ------------------------------------------------------------------------------------------
+
+export CONTENTFUL_MANAGEMENT_HOSTNAME=cdn.contentful.com
+export CONTENTFUL_SPACE_ID=
+export CONTENTFUL_ACCESS_TOKEN=
+
+# EOF
