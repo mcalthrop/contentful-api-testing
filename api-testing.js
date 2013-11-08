a87baa5f70e74e0009a09bc68117964f559ce04c
Matt Calthrop <mcalthrop@gmail.com>
2013-11-08 13:30:54 +0000

    Changed env var check to fail on blank var

--- api-testing.js
+++ api-testing.js
@@ -6,18 +6,19 @@
 
     /**
      * Utility function to retrieve the value of an environment variable, and optionally
-     * throw an error if the specified variable has not been set.
+     * throw an error if the specified variable has not been set or is blank.
      *
      * @param {String} varName name of environment variable to retrieve
-     * @param {Boolean} throwErrorIfUndefined if the specified variable has not been defined
+     * @param {Boolean} throwErrorIfEmpty if the specified variable is empty - ie,
+     *          has not been defined, or is blank
      *
      * @returns {String} the value of the specified env variable
      */
-    function getEnv(varName, throwErrorIfUndefined) {
+    function getEnv(varName, throwErrorIfEmpty) {
         var varValue = process.env[varName];
 
-        if (varValue === undefined && throwErrorIfUndefined) {
-            throw new ReferenceError('Environment variable is not defined: ' + varName);
+        if ((varValue === undefined || (!varValue)) && throwErrorIfEmpty) {
+            throw new ReferenceError('Environment variable is empty: ' + varName);
         }
 
         return varValue;
